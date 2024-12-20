37297fc958e74cf58cab33d54fa21415af9bf656
Gökmen Görgen <gkmngrgn@gmail.com>
2024-12-20 01:44:27 +0100

    better example for our form.

--- evet-web/src/main.rs
+++ evet-web/src/main.rs
@@ -1,9 +1,7 @@
 use leptos::prelude::*;
-use stylers::style;
-
 
 fn main() {
-    leptos::mount::mount_to_body(App)
+    leptos::mount::mount_to_body(Home)
 }
 
 #[component]
@@ -11,13 +9,25 @@ fn Home() -> impl IntoView {
     let (count, set_count) = signal(0);
 
     view! {
-        <main class="my-0 mx-auto max-w-3xl text-center">
-            <h2 class="p-6 text-4xl">"Welcome to Leptos with Tailwind"</h2>
-            <p class="px-10 pb-10 text-left">"Tailwind will scan your Rust files for Tailwind class names and compile them into a CSS file."</p>
-            <button
-                class="bg-sky-600 hover:bg-sky-700 px-5 py-3 text-white rounded-lg"
-                on:click=move |_| *set_count.write() += 1
-            >
+        <main class="event-form">
+            <div class="event-form__input">
+                <label for="message">Message</label>
+                <input type="text" id="message" />
+            </div>
+            <input type="text" placeholder="Enter your message" />
+            <input type="date" />
+            <input type="time" />
+            <select>
+                <option value="America/New_York">America/New_York</option>
+                <option value="America/Los_Angeles">America/Los_Angeles</option>
+                <option value="America/Chicago">America/Chicago</option>
+                <option value="America/Denver">America/Denver</option>
+                <option value="America/Phoenix">America/Phoenix</option>
+                <option value="America/Anchorage">America/Anchorage</option>
+                <option value="Pacific/Honolulu">Pacific/Honolulu</option>
+            </select>
+            <button>Submit</button>
+            <button on:click=move |_| *set_count.write() += 1>
                 {move || if count.get() == 0 {
                     "Click me!".to_string()
                 } else {
@@ -25,42 +35,5 @@ fn Home() -> impl IntoView {
                 }}
             </button>
         </main>
-    }
-}
-
-#[component]
-pub fn App() -> impl IntoView {
-    let styler_class = style! { "App",
-        #two{
-            color: blue;
-        }
-        div.one{
-            color: red;
-            content: raw_str(r#"\hello"#);
-            font: "1.3em/1.2" Arial, Helvetica, sans-serif;
-        }
-        div {
-            border: 1px solid black;
-            margin: 25px 50px 75px 100px;
-            background-color: lightblue;
-        }
-        h2 {
-            color: purple;
-        }
-        @media only screen and (max-width: 1000px) {
-            h3 {
-                background-color: lightblue;
-                color: blue
-            }
-        }
-    };
-
-    view! { class = styler_class,
-        <div class="one">
-            <h1 id="two">"Hello"</h1>
-            <h2>"World"</h2>
-            <h2>"and"</h2>
-            <h3>"friends!"</h3>
-        </div>
     }
 }
